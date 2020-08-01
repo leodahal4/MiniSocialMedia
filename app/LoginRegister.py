a8d0ea18325d44c05ec5bbe1cee5ad6d20fb159c
Leo <leodahal4@gmail.com>
2020-08-01 16:31:06 +0545

    login register and post model ready

---
+++ app/LoginRegister.py
@@ -0,0 +1,12 @@
+# this is the model for Login and register form
+
+
+class LoginRegister:
+    def __init__(self):
+        pass
+
+    def login(self):
+        pass
+
+    def register(self):
+        pass
---
+++ app/Post.py
@@ -0,0 +1,6 @@
+# this is the model for the post.
+
+
+class Post:
+    def __init__(self):
+        pass
